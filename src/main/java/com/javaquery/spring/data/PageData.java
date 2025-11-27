8bd19ef8f2cfc71d82b0bd06dbdf26ec7bd4f99a
javaquery <vicky.thakor@javaquery.com>
2025-11-27 14:59:22 +0530

    Refactor PageData to a class with explicit getters

--- src/main/java/com/javaquery/spring/data/PageData.java
+++ src/main/java/com/javaquery/spring/data/PageData.java
@@ -6,5 +6,38 @@ import java.util.List;
  * @author javaquery
  * @since 2025-11-26
  */
-public record PageData<T>(long totalElements, int totalPages, int currentPage, int pageSize, List<T> data) {
+public class PageData<T> {
+    private final long totalElements;
+    private final int totalPages;
+    private final int currentPage;
+    private final int pageSize;
+    private final List<T> data;
+
+    public PageData(long totalElements, int totalPages, int currentPage, int pageSize, List<T> data) {
+        this.totalElements = totalElements;
+        this.totalPages = totalPages;
+        this.currentPage = currentPage;
+        this.pageSize = pageSize;
+        this.data = data;
+    }
+
+    public long getTotalElements() {
+        return totalElements;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public List<T> getData() {
+        return data;
+    }
 }
